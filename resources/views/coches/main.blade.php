42eb9ba7e0310791772c7bdde0c8c8c97369c634
Carlos Manso <carlos.manso.fp@iescampanillas.com>
2023-02-28 11:07:59 +0100

    Duda sobre error en variable $datos

--- resources/views/coches/main.blade.php
+++ resources/views/coches/main.blade.php
@@ -2,22 +2,45 @@
 
 @section("main")
 
+    @empty($datos)
+
+        <div class="bg-red-300">
+            No hay datos en la base de datos.
+        </div> 
+
+    @else
 
         <div class="bg-white-900">
         
-                    {{-- @foreach($datos as $coche) --}}
-                    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <table class="my-8 text-justify m-auto bg-gray-300" style="width: 80%;">
+                <thead class="bg-gray-800 text-stone-50 text-center">
+                    <tr>
+                        <th class="p-3" style="width:20rem;">@lang("app.etq_receta")</th>
+                        <th style="width: 30rem;">@lang("app.etq_descripcion")</th>
+                        <th class="font-bold" style="width: 10rem;">@lang("app.etq_fecha")</th>
+                        <th style="width:1rem;"></th>
+                        <th style="width:1rem;"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($datos as $coche)
+                            <td style="padding: 15px" class="text-center">{{ $coche->marca }}</td>
+                            <td style="padding: 15px">{{ $coche->modelo }}</td>
+                            <td style="padding: 15px" class="text-center">{{ $coche->precio }}</td>
+                            <td style="padding: 15px">
+                                <a href="">EDITAR </a>
+                            </td>
+                            <td style="padding: 10px">
+                                <a href=" {{route("coche.borrar", ["id" => $coche->idCoc])}} ">BORRAR</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
 
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">MERCEDES-BENZ</h5>
-
-                        <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-
-                    </div>
-                    {{-- @endforeach --}}
-
-        {{-- <h1 class="m-9 text-2xl text-center">@lang("app.etq_tienes"){{ trans_choice("app.etq_total", $datos->count(), ["num" => $datos->count()]) }}</h1> --}}
+        </table>
 
         </div> 
 
+    @endempty
 
 @endsection
